74c97307d8d4855c9674afe2b103f3a75d5abca2
kimgunwoocode <105213943+kimgunwoocode@users.noreply.github.com>
2025-05-09 18:14:05 +0900

    .

--- Assets/Script/Title/title_manager.cs
+++ Assets/Script/Title/title_manager.cs
@@ -16,20 +16,32 @@ public class title_manager : MonoBehaviour
     public Button Setting_button;
     public Button Quit_button;
 
-    public Scene StartScene;
-    public Scene TutorialScene;
+    [Header("Scence")]
+    public string StartScene;
+    public string TutorialScene;
+
+    private void Start()
+    {
+        Book_screen.SetActive(false);
+        Setting_screen.SetActive(false);
+        Quit_screen.SetActive(false);
+    }
 
     public void StartButton_function()
     {
-        
+        SceneManager.LoadScene(StartScene);
     }
 
     public void OpenAndClose_BookScreen()
     {
-
+        Book_screen.SetActive(Book_screen.activeSelf);
     }
     public void OpenAndClose_SettingScreen()
     {
-
+        Setting_screen.SetActive(Setting_screen.activeSelf);
+    }
+    public void OpenAndClose_QuitScreen()
+    {
+        Quit_screen.SetActive(Quit_screen.activeSelf);
     }
 }
